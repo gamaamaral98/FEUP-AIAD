8f4ad7da3cf643c8e1f11c35f9528725fb03deb7
TudoOBentoLBAW <up201708805@fe.up.pt>
2019-10-16 12:00:12 +0100

    Agents created

--- AIAD_Project/src/Agents/ATMs.java
+++ AIAD_Project/src/Agents/ATMs.java
@@ -1,4 +1,11 @@
 package Agents;
+import jade.core.AID;
+import jade.core.Agent;
 
-public class ATMs {
+public class ATMs extends Agent {
+    protected void setup() {
+        //String nickname = "ATM";
+        //AID id = new AID(nickname, AID.ISLOCALNAME);
+        System.out.println("Hello! ATM-Agent " + getAID().getName() + " is ready!");
+    }
 }
--- AIAD_Project/src/Agents/Clients.java
+++ AIAD_Project/src/Agents/Clients.java
@@ -1,4 +1,11 @@
 package Agents;
+import jade.core.AID;
+import jade.core.Agent;
 
-public class Clients {
+public class Clients extends Agent {
+    protected void setup() {
+        //String nickname = "ATM";
+        //AID id = new AID(nickname, AID.ISLOCALNAME);
+        System.out.println("Hello! Client-Agent " + getAID().getName() + " is ready!");
+    }
 }
